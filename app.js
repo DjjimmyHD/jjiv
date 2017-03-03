cdabc9a837e0d5cd2bc2622f9b6ca3025912c056
Jackson Donovan <iinventedtwitter@gmail.com>
2017-03-03 14:16:10 -0700

    Added functionality to one button

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function(){
+  $('.butt').click(function(){
+    grabAww()
+  })
+})
+function grabAww(){
+  $.get('https://galvanize-cors.herokuapp.com/http://www.reddit.com/r/pics/.json', function (data) {
+    var placeholder = document.querySelectorAll('.placeholders')
+    console.log(placeholder.length)
+    for(var i = 0; i < placeholder.length; i++){
+      var thisIsTheUrl = data.data.children[i].data.url
+      placeholder[i].setAttribute('src',thisIsTheUrl);
+      console.log(thisIsTheUrl);
+    }
+  })
+}
